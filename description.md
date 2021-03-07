4c8a13e9657eed921149f7f387995b0db5c54074
ILYA <noskoviv66@gmail.com>
2021-03-07 14:09:18 +0500

    my

---
+++ description.md
@@ -0,0 +1,3 @@
+# HELLO World
+
+**Hero is bold**
---
+++ index.js
@@ -0,0 +1,11 @@
+const getUser = async () = {
+  try {
+  const res = await fetch('vk.com')
+  const data = await res.json()
+    
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+getUser()
